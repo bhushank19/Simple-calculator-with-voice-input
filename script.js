ae8e7642799107dfc31ccec7b9756f42202e015a
Bhushan Kale <bhushanpkale@gmail.com>
2021-03-02 19:03:15 +0530

    By Number

--- script.js
+++ script.js
@@ -66,3 +66,13 @@ for(var i =0;i<operator.length;i++){
 		
 	});
 }
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
+}
