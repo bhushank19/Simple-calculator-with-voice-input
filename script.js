55b7b06b319ed3fa699766682f9913827db8a41b
Bhushan Kale <bhushanpkale@gmail.com>
2021-03-02 19:03:46 +0530

    By Microphone

--- script.js
+++ script.js
@@ -76,3 +76,30 @@ for(var i =0;i<number.length;i++){
 		}
 	});
 }
+var microphone = document.getElementById('microphone');
+microphone.onclick=function(){
+	microphone.classList.add("record");
+	var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+	recognition.lang = 'en-US';
+	recognition.start();
+	operations = {"plus":"+",
+				 "minus":"-",
+				 "multiply":"*",
+				 "multiplied":"*",
+				 "divide":"/",
+				 "divided":"/",
+				 "reminder":"%"}
+	
+	recognition.onresult = function(event){
+		var input = event.results[0][0].transcript;
+		for(property in operations){
+			input= input.replace(property, operations[property]);
+		}
+		document.getElementById("output-value").innerText = input;
+		setTimeout(function(){
+			evaluate(input);
+		},2000);
+		microphone.classList.remove("record");
+	}
+	
+}
